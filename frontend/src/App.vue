057e36760068dfc0b4b4e86be1e16a02d91949de
Meshu Deb Nath <meshu.uiu@gmail.com>
2021-02-06 00:14:17 +0100

    frontend added

---
+++ frontend/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row vertical-centre justify-content-center mt-50">
+      <div class="col-md-6 mx-auto">
+        <br>
+        <CreateTodo></CreateTodo>
+        <ListTodo></ListTodo>
+        <br>
+        <p>It's a demo project for Seminar Web Engineering. Topic: "Kubernetes-111"</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CreateTodo from "./components/CreateTodo.vue";
+import ListTodo from "./components/ListTodo.vue";
+
+export default {
+  name: "app",
+  components: { CreateTodo, ListTodo }
+};
+</script>
+
+<style lang="scss">
+@import "node_modules/bootstrap/scss/bootstrap";
+@import "node_modules/bootstrap-vue/src/index.scss";
+.vertical-centre {
+  min-height: 100%;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+}
+.todo__row {
+  width: 400px;
+}
+</style>
